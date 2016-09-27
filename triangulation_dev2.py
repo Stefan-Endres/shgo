8bed6be38a6ec0d4b759e355331a1fcf62ab6526
stefan-endres <stefan.c.endres@gmail.com>
2016-09-27 17:15:37 +0200

    Cell homology groups

--- triangulation_dev2.py
+++ triangulation_dev2.py
@@ -24,46 +24,45 @@ class Cell:
         """
         Returns the homology group of the current cell
         """
-        pass
+        hg_n = 0
+        for v in self.C:
+            if v.minimiser():
+                hg_n += 1
+
+        self.hg_n = hg_n
+        return hg_n
 
     def homology_group_differential(self):
         """
         Returns the difference between the current homology group of the
         cell and it's parent group
         """
-        pass
+        self.hgd = self.hg_n - self.hg
 
-    def sperner_polytope_lemma(self):
+    def polytopial_sperner_lemma(self):
         """
         Returns the number of stationary points theoretically contained in the
         cell based information currently known about the cell
         """
         pass
-    
+
 class Complex:
     def __init__(self, dim, func, func_args=(), symmetry=False, g_cons=None):
         self.dim = dim
         self.gen = 0
         self.perm_cycle = 0
-        # List of cells
-        self.C = []
-        #self.cell = 0
-        # ^ Put every vertex into its own cell which changes
-        # every generation
 
-        # We set self.V to be the list of vertices in every generation
-        # So self.V[0] is the list of vertices in the initial cube
-        # generation
-        # self.V[1] = [(0.5, 0.5, 0.5, ...)]
-        # self.V[2] is the second cycle permutation split
-        # etc.
-        self.V = VertexCached(func, func_args)
+        self.C = []  # List of cells
+        self.V = VertexCached(func, func_args)  # Cache of all vertices
 
         # Generate n-cube here:
         self.n_cube(dim, symmetry=symmetry)
         self.C.append(self.C0)
+        hg0 = self.C0.homology_group()
+        #for v in self.C0():
+         #   print(v)
 
-    def n_cube(self, dim, symmetry=False, printout=True):
+    def n_cube(self, dim, symmetry=False, printout=False):
         """
         Generate the simplicial triangulation of the n dimensional hypercube
         containing 2**n vertices
@@ -121,11 +120,15 @@ class Complex:
             self.perm(i2_parents, xi2)
 
 
+
+
 class Vertex:
     def __init__(self, x, func=None, func_args=(), nn=None):
         import numpy
         self.x = x
         x_a = numpy.array(x)
+        # Note Vertex is only initiate once for all x so only
+        # evaluated once
         self.f = func(x_a, *func_args)
         if nn is not None:
             self.nn = nn
@@ -133,14 +136,34 @@ class Vertex:
             self.nn = []
 
         self.fval = None
+        self.check_min = True
 
     def connect(self, v):
         if v not in self.nn and v is not self:  # <-- Cool
             self.nn.append(v)
+            if self.f > v.f:
+                self.min = False
+            #self.check_min = True
 
     def disconnect(self, v):
         if v in self.nn:
             self.nn.remove(v)
+            self.check_min = True
+
+    def minimiser(self):
+        if self.check_min:
+            # Check if the current vertex is a minimiser
+            self.min = False
+            for v in self.nn:
+                if self.f > v.f:
+                    break
+
+                self.min = True
+
+            self.check_min = False
+            return (self.min)
+        else:
+            return(self.min)
 
 class VertexCached:
     def __init__(self, func, func_args):
@@ -155,7 +178,6 @@ class VertexCached:
             return self.cache[x]
         else:
             xval = Vertex(x, func=self.func, func_args=self.func_args)
-            #xval = Vertex(x)
             logging.info("New generated vertex at x = {}".format(x))
             self.cache[x] = xval
             return xval
@@ -165,6 +187,6 @@ class VertexCached:
 if __name__ == '__main__':
     def test_func(x):
         import numpy
-        return numpy.sum(x ** 2)
+        return numpy.sum(x ** 2) + 2.0 * x[0]
 
     HC = Complex(3, test_func)
