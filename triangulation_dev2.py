fd872640e3e86b1e8709745c7ec3d1150cf18c72
stefan-endres <stefan.c.endres@gmail.com>
2017-01-14 09:01:22 +0200

    Sub triangulations

--- triangulation_dev2.py
+++ triangulation_dev2.py
@@ -10,14 +10,18 @@ class Cell:
     """
     Contains a cell that is symmetric to the initial hypercube triangulation
     """
-    def __init__(self, gen, hg):
-        self.gen = gen  # generation
-        self.hg = hg  # parent homology group
+    def __init__(self, gen, hgr, origin, suprenum):
+        self.gen = gen  # parent generation
+        self.hgr = hgr  # parent homology group rank
         self.C = []
+        self.origin = origin
+        self.suprenum = suprenum
+        self.centroid = None  # (Not always used)
         #TODO: self.bounds
 
     def __call__(self):
         return self.C
+
 
     def add_vertex(self, V):
         if V not in self.C:
@@ -53,23 +57,41 @@ class Cell:
         """
         Print the current cell to console
         """
-        return
+        for v in self():
+            print("Vertex: {}".format(v.x))
+            constr = 'Connections: '
+            for vc in v.nn:
+                constr += '{} '.format(vc.x)
+
+            print(constr)
+            print('Order = {}'.format(v.order))
 
 class Complex:
     def __init__(self, dim, func, func_args=(), symmetry=False, g_cons=None):
         self.dim = dim
-        self.gen = 1
+        self.gen = 0
         self.perm_cycle = 0
 
-        self.C = []  # List of cells
+        # Every cell is stored in a list of its generation,
+        # ex. the initial cell is stored in self.H[0]
+        # 1st get new cells are stored in self.H[1] etc.
+        # When a cell is subgenerated it is removed from this list
+
+        self.H = []  # Storage structure of cells
+
         self.V = VertexCached(func, func_args)  # Cache of all vertices
 
         # Generate n-cube here:
         self.n_cube(dim, symmetry=symmetry)
-        self.C.append(self.C0)
+        self.H.append([])
+        self.H[0].append(self.C0)
         self.hg0 = self.C0.homology_group_order()
         #for v in self.C0():
          #   print(v)
+
+    def __call__(self):
+        return self.H
+
 
     def n_cube(self, dim, symmetry=False, printout=False):
         """
@@ -81,7 +103,7 @@ class Complex:
         self.origin = origin
         supremum = list(numpy.ones(dim, dtype=int))
         self.suprenum = supremum
-        self.C0 = Cell(0, 0)  # Initial cell object
+        self.C0 = Cell(0, 0, self.origin, self.suprenum)  # Initial cell object
         self.C0.add_vertex(self.V(tuple(origin)))
         self.C0.add_vertex(self.V(tuple(supremum)))
 
@@ -136,6 +158,7 @@ class Complex:
         a cell and add the new vertex to the complex"""
         self.centroid = list((numpy.array(self.origin) + numpy.array(self.suprenum))/2.0)
         self.C0.add_vertex(self.V(tuple(self.centroid)))
+        self.C0.centroid = self.centroid
 
         # Disconnect origin and suprenum
         self.V(tuple(self.origin)).disconnect(self.V(tuple(self.suprenum)))
@@ -174,7 +197,111 @@ class Complex:
                 self.graph[i].append(v2.I)
 
 
+    # Graph structure method:
+    # 0. Capture the indices of the initial cell.
+    # 1. Generate new origin and suprenum scalars based on current generation
+    # 2. Generate a new set of vertices corresponding to a new
+    #    "origin" and "suprenum"
+    # 3. Connected based on the indices of the previous graph structure
+    # 4. Disconnect the edges in the original cell
 
+    def sub_generate_cell(self, C_i, gen, hgr):
+        """Subgenerate a cell `C_i` of generation `gen` and
+        homology group rank `hgr`."""
+        origin_new = C_i.centroid
+        centroid_index = len(C_i()) - 1
+
+        # If not gen append
+        try:
+            self.H[gen]
+        except IndexError:
+            self.H.append([])
+
+
+        # Generate subcubes using every extreme verex in C_i as a suprenum
+        # and the centroid of C_i as the origin
+        for i, v in enumerate(C_i()):
+            #origin = tuple(C_i.centroid)
+            if i is not centroid_index:
+                suprenum = tuple(v.x)
+                self.construct_hypercube(origin_new, suprenum, gen, hgr)
+
+        # Disconnected all edges of parent cells (except origin to sup)
+        for i, connections in enumerate(self.graph):
+            # Present vertex V_new[i]; connect to all connections:
+            if i == centroid_index:  # Break out of centroid
+                break
+
+            for j in connections:
+                if j is not centroid_index:  # (connect to all new vertices)
+                    #print('C_i()[i] = {}'.format(C_i()[i]))
+                    C_i()[i].disconnect(C_i()[j])
+                    #self.V(tuple(C_i()[i])).disconnect(self.V(tuple(C_i()[j])))
+
+        #TODO: Recalculate all the homology group ranks of each cell
+        return
+
+
+    def construct_hypercube(self, origin, suprenum, gen, hgr, printout=False):
+        """
+        Build a hypercube with triangulations symmetric to C0.
+
+        Parameters
+        ----------
+        origin : vec
+        suprenum : vec
+        gen : generation
+        hgr : parent homology group rank
+        """
+
+        # Initiate new cell
+        C_new = Cell(gen, hgr, origin, suprenum)
+        C_new.centroid = list((numpy.array(origin) + numpy.array(suprenum))/2.0)
+
+        centroid_index = len(HC.C0()) - 1
+        # Build new indexed vertex list
+        #sup = list(suprenum)
+        V_new = []
+
+        for i, v in enumerate(HC.C0()):
+            if i == centroid_index:  # (This should be the last index of HC.C0()
+                C_new.add_vertex(self.V(tuple(C_new.centroid)))
+                V_new.append(C_new.centroid)
+                break
+
+            vec = list(origin)  # set a vec equal to origin tuple
+            for j, x_i in enumerate(v.x):
+                if x_i:  # (if x_i = 1, else leave x_i = 0 at origin
+                    vec[j] = suprenum[j]
+
+            C_new.add_vertex(self.V(tuple(vec)))
+            V_new.append(vec)
+
+        # Connect new vertices
+        for i, connections in enumerate(self.graph):
+            # Present vertex V_new[i]; connect to all connections:
+            for j in connections:
+                self.V(tuple(V_new[i])).connect(self.V(tuple(V_new[j])))
+
+        #print('V_new = {}'.format(V_new))
+
+        if printout:
+            print("A sub hyper cube with:")
+            print("origin: {}".format(origin))
+            print("suprenum: {}".format(suprenum))
+            for v in C_new():
+                print("Vertex: {}".format(v.x))
+                constr = 'Connections: '
+                for vc in v.nn:
+                    constr += '{} '.format(vc.x)
+
+                print(constr)
+                print('Order = {}'.format(v.order))
+
+        # Append the new cell to the to complex
+        self.H[gen].append(C_new)
+
+        return C_new
 
     # Not completed zone:
     ## Symmetry group topological transformation methods
@@ -324,11 +451,35 @@ if __name__ == '__main__':
     #Complex.generate_gen(HC)
 
     HC.add_centroid()
-    HC.incidence()
-    print(HC.structure)
+
+    if 0:
+        HC.incidence()
+        print(HC.structure)
+
     HC.graph_map()
-    print(HC.graph)
+    print('HC.graph = {}'.format(HC.graph))
     #print(HC.C0())
+
+
+    print(HC())
+
+    #HC.sub_generate_cell()
+
+    origin = (0.5, 0.5, 0.5, 0.5)
+    suprenum = (1.0, 1.0, 1.0, 1.0)
+    gen = 1
+    hgr = 1
+    HC.H.append([])
+
+    HC.sub_generate_cell(HC.C0, gen, hgr)
+    #HC.construct_hypercube(origin, suprenum, gen, hgr)
+
+
+    if 1:
+        print(HC.H)
+        print(len(HC.H[1]))
+        print(HC.H[1][0])
+        HC.H[1][0].print_out()
 
     if 0:
         print(HC.V((0.5, 0.5, 0.5, 0.5)).nn)
