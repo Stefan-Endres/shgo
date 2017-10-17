c3a0765ad3bce97b741eb3a018465cc42a7493f3
stefan-endres <stefan.c.endres@gmail.com>
2017-10-17 13:39:13 +0200

    Add installation files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='shgo',
+      version='0.1',
+      description='Simplicial homology global optimisation',
+      url='https://github.com/stefan-endres/shgo',
+      author='stefan-endres',
+      author_email='stefan.c.endres@gmail.com',
+      license='MIT',
+      packages=['shgo'],
+      install_requires=[
+          'scipy',
+          'numpy'
+      ],
+      test_suite='./shgo_tests.py',
+      zip_safe=False)
