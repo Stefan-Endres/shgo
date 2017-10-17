da72b1c11f61e381938e5f88be9589e86f057974
stefan-endres <stefan.c.endres@gmail.com>
2017-10-17 14:57:42 +0200

    Add setup details

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='shgo',
-      version='0.1.3',
+      version='0.3.2',
       description='Simplicial homology global optimisation',
       url='https://github.com/stefan-endres/shgo',
       author='stefan-endres',
@@ -11,6 +11,29 @@ setup(name='shgo',
       install_requires=[
           'scipy',
           'numpy'
+      ],
+      keywords='optimization',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+           'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Mathematics',
+
+          # Pick your license as you wish (should match "license" above)
+           'License :: OSI Approved :: MIT License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ],
       test_suite='shgo.shgo_tests',
       zip_safe=False)
